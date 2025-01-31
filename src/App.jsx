a85018f99fe05a40e3e0459b423855828eb9d6cb
eleonoracappellesso <cappellessoeleonora@gmail.com>
2025-01-31 16:25:34 +0100

    created routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import DefaultLayout from "./layout/DefaultLayout";
+import TravelListPage from "./pages/TravelListPage";
+import TravelDetailsPage from "./pages/TravelDetailsPage";
+import NotFound from "./pages/NotFound";
 
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route Component={DefaultLayout}>
+          <Route path="/travels">
+            <Route index Component={TravelListPage} />
+            <Route path="/details" Component={TravelDetailsPage} />
+          </Route>
+        </Route>
+        <Route path="*" Component={NotFound} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
