505268326bcead5ab3e374a5941b3e858d8c3989
eleonoracappellesso <cappellessoeleonora@gmail.com>
2025-01-31 18:12:25 +0100

    fixed router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import GlobalProvider from "./context/GlobalContext";
 import DefaultLayout from "./layout/DefaultLayout";
 import TravelListPage from "./pages/TravelListPage";
 import TravelDetailsPage from "./pages/TravelDetailsPage";
@@ -9,15 +10,19 @@ function App() {
 
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/travels" element={<DefaultLayout />}>
-          <Route index element={<TravelListPage />} />
-          <Route path="/:id" element={<TravelDetailsPage />} />
-        </Route>
-        <Route path="*" Component={NotFound} />
-      </Routes>
-    </BrowserRouter>
+    <GlobalProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/travels" Component={<DefaultLayout />}> */}
+          <Route path="/travels">
+            <Route index Component={<TravelListPage />} />
+            <Route path="/:id" Component={<TravelDetailsPage />} />
+          </Route>
+          {/* </Route> */}
+          <Route path="*" Component={NotFound} />
+        </Routes>
+      </BrowserRouter>
+    </GlobalProvider>
   )
 }
 
